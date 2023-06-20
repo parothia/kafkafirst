38e299f464d67ef93788a9d316f5caf6f4758085
parothia <anurag786paro@gmail.com>
2023-06-21 00:59:48 +0530

    producing and sending messages-working-fine

---
+++ src/main/java/com/kafkafirst/kafkafirst/KafkafirstApplication.java
@@ -0,0 +1,13 @@
+package com.kafkafirst.kafkafirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkafirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkafirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkafirst/kafkafirst/KafkafirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkafirst.kafkafirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkafirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
